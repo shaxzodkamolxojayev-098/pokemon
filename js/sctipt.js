42363669ad0ba8ca6c6b99e302aaddf9d61fc06b
shaxzodkamolxojayev-098 <shaxzodkamolxojayev@gmail.com>
2022-10-06 14:07:49 +0500

    initial commit

---
+++ js/sctipt.js
@@ -0,0 +1,14 @@
+"use strict"
+console.log(pokemons);
+
+
+// let wrapper=document.querySelector('.wrapper');
+
+
+// const card=document.createElement('div');
+// card.setAttribute('class','card');
+// card.innerHTML='javaScript';
+
+console.log($qs('.wrapper'));
+
+$qs('.wrapper').style.color = "red";
---
+++ plugin.js
@@ -0,0 +1,8 @@
+const $qs=function(element){
+  return document.querySelector(element)
+}
+
+
+const $qsAll=function(element){
+  return document.querySelectorAll(element)
+}
